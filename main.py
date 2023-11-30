32e49b3848badfaa112e8fc29774b7c7fddc21ea
Ming <ming2007119@gmail.com>
2023-11-30 16:36:05 +0800

    idct/quantize

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 import sys
 import numpy as np
 
+def idct2d(input: np.ndarray) -> np.ndarray:
+  l = image.shape[0]
+  c = np.zeros((l, l))
+  for k in range(l):
+    for n in range(l):
+      c[k, n] = np.sqrt(1/l) * np.cos(np.pi*k*(1/2+n)/l)
+      if k != 0:
+        c[k, n] *= np.sqrt(2)
+  return (np.kron(c, c) @ input.flatten()).reshape((l, l))
+
+def quantize(input: np.ndarray, q: np.ndarray) -> np.ndarray:
+  return input // q
+
 if __name__ == "__main__":
   if (len(sys.argv) != 2):
     print("Usage: python main.py <input>")
@@ -14,5 +27,3 @@ if __name__ == "__main__":
   with open(input, "rb") as jpegfile:
     image = np.frombuffer(jpegfile.read(), dtype=np.uint8)
   
-  print(image)
-  print(image.shape)
