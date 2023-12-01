fb8ab375b4b05b648ec17815cf84d4e3740aa695
Ming <ming2007119@gmail.com>
2023-12-01 17:20:29 +0800

    WIP: refactoring TODO: SOS

--- utils.py
+++ utils.py
@@ -1,28 +1,16 @@
+from typing import List
 
-zigzag: list = list([
-  [  0,  1,  5,  6, 14, 15, 27, 28, ],
-  [  2,  4,  7, 13, 16, 26, 29, 42, ],
-  [  3,  8, 12, 17, 25, 30, 41, 43, ],
-  [  9, 11, 18, 24, 31, 40, 44, 53, ],
-  [ 10, 19, 23, 32, 39, 45, 52, 54, ],
-  [ 20, 22, 33, 38, 46, 51, 55, 60, ],
-  [ 21, 34, 37, 47, 50, 56, 59, 61, ],
-  [ 35, 36, 48, 49, 57, 58, 62, 63  ]
-])
-
-marker_mapping: dict = {
-  0xFFD8: "Start of Image (SOI)",
-  0xFFE0: "Application Segment 0 (APP0)",
-  0xFFDB: "Define Quantization Table (DQT)",
-  0xFFC0: "Start of Frame (SOF0)",
-  0xFFC4: "Define Huffman Table (DHT)",
-  0xFFDA: "Start of Scan (SOS)",
-  0xFFD9: "End of Image (EOI)",
-  
-  0xFFFE: "Comment (COM)",
-  0xFFE2: "Application Segment 2 (APP2)",
-  0xFFC2: "Start of Frame (SOF2)",
-}
+MARKERS: List = [
+  "SOF0", "SOF1", "SOF2", "SOF3", "DHT", "SOF5", "SOF6", "SOF7",
+  "JPG", "SOF9", "SOF10", "SOF11", "DAC", "SOF13", "SOF14", "SOF15",
+  "RST0", "RST1", "RST2", "RST3", "RST4", "RST5", "RST6", "RST7",
+  "SOI", "EOI", "SOS", "DQT", "DNL", "DRI", "DHP", "EXP",
+  "APP0", "APP1", "APP2", "APP3", "APP4", "APP5", "APP6", "APP7",
+  "APP8", "APP9", "APP10", "APP11", "APP12", "APP13", "APP14", "APP15",
+  "JPG0", "JPG1", "JPG2", "JPG3", "JPG4", "JPG5", "JPG6", "JPG7",
+  "JPG8", "JPG9", "JPG10", "JPG11", "JPG12", "JPG13", "COM", "TEM",
+  "[Invalid Marker]"
+]
 
 subsample_mapping: dict = {
   "11": "4:4:4",
