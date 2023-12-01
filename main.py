4f198a2bf41c96174939d1502c76599828265fc5
Ming <ming2007119@gmail.com>
2023-12-01 17:19:39 +0800

    WIP: refactoring main

--- main.py
+++ main.py
@@ -4,15 +4,15 @@ import matplotlib.pyplot as plt
 from JPEG import JPEG
 
 if __name__ == "__main__":
-  if (len(sys.argv) != 2):
+  if (len(sys.argv) < 2):
     print("Usage: python main.py <input>")
     exit(1)
 
-  input = sys.argv[1]
-  if (not input.endswith(".jpg") and not input.endswith(".jpeg")):
+  input_path = sys.argv[1]
+  output_path = sys.argv[2] if len(sys.argv) > 2 else None
+  if (not input_path.endswith(".jpg") and not input_path.endswith(".jpeg")):
     print("Input file must be a JPEG image")
     exit(1)
   
-  jpeg = JPEG(input)
+  jpeg = JPEG(input_path, output_path)
   jpeg.decode()
-  jpeg.save("output.bmp")
